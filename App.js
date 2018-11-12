34d78f305e58a880970407fa14a8231cf5cabde1
Jeremy Knight <jknight42@yahoo.com>
2018-11-12 15:57:27 -0800

    Big ugly commit of a bunch of random stuff, but I fixed the issue with having moveAction be recursive. It was crashing, but it was because I wasn't incrementing the variable correctly.

--- App.js
+++ App.js
@@ -1,73 +1,93 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button, ScrollView } from 'react-native';
-import DeckOfCards from './DeckOfCards.js';
-import PlayingCard from './PlayingCard.js';
-import PlayerList from './PlayerList.js';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  ScrollView,
+  TouchableOpacity
+} from "react-native";
+import PlayingCard from "./PlayingCard.js";
+import PlayerList from "./PlayerList.js";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default class App extends React.Component {
+  // currDeal is changed each time a new set of cards are dealt to all players.
+  // currRound is changed for each round of betting: preflop, flop, turn, river.
 
   state = {
-    roundInfo: {
-      currentRound: "preflop",
-      dealerSeat: 0,
-      bigBlindSeat: 1,
-      smallBlindSeat: 2, 
-      highestBet: 0, 
+    currDealInfo: {
+      currRound: "pregame",
+      whoseTurnIsIt: 3,
+      highestBet: 0,
+      currPotRound: 0,
+      currPotTotal: 0,
+      sharedCards: []
     },
     deckOfCards: [],
     players: [
       {
         key: 0,
-        name: "Jeremy",
-        chipStack: 100,
+        name: "Jeremy        ",
+        chipStack: 50,
         isAi: false,
         isUser: true,
         currentHand: [],
         betThisRound: 0,
+        positionThisDeal: 0,
+        isItMyTurn: false,
+        hasFolded: false
       },
       {
         key: 1,
-        name: "Tex Bettor",
-        chipStack: 100,
+        name: "Tex Bettor    ",
+        chipStack: 50,
         isAi: true,
         isUser: false,
         currentHand: [],
         betThisRound: 0,
+        positionThisDeal: 1,
+        isItMyTurn: false,
+        hasFolded: false
       },
       {
         key: 2,
-        name: "Juan Suave",
-        chipStack: 100,
+        name: "Juan Suave    ",
+        chipStack: 50,
         isAi: true,
         isUser: false,
         currentHand: [],
         betThisRound: 0,
+        positionThisDeal: 2,
+        isItMyTurn: false,
+        hasFolded: false
       },
       {
         key: 3,
         name: "Chip Fullhouse",
-        chipStack: 100,
+        chipStack: 50,
         isAi: true,
         isUser: false,
         currentHand: [],
         betThisRound: 0,
+        positionThisDeal: 3,
+        isItMyTurn: false,
+        hasFolded: false
       }
-    ],
-    numOfPlayers: 8,
-  }
+    ]
+  };
 
   constructor(props) {
     super(props);
 
     this.createDeckOfCards();
-
   }
   createDeckOfCards = () => {
     // Create all cards
     let currCardNum = 0;
     const maxCardValue = 14;
-    const suits = ["spade","diamond","club","heart"];
-    
+    const suits = ["spade", "diamond", "club", "heart"];
+
     let deckOfCards = [];
 
     for (var suitIndex = 0; suitIndex < suits.length; suitIndex++) {
@@ -76,53 +96,300 @@ export default class App extends React.Component {
           key: currCardNum,
           suit: suits[suitIndex],
           value: valueIndex,
-          hasBeenDealt: false,
-        }
+          hasBeenDealt: false
+        };
         currCardNum++;
       }
     }
-    
+
     deckOfCards = shuffle(deckOfCards);
-    
-    this.state = {
-      ...this.state,
-      deckOfCards: deckOfCards,
-    }
-  }
+
+    this.state = { ...this.state, deckOfCards: deckOfCards };
+  };
   dealCardsToPlayers = () => {
     let players = this.state.players;
     for (let playerIndex = 0; playerIndex < players.length; playerIndex++) {
       players[playerIndex].currentHand[0] = this.getNextCardFromTheDeck();
       players[playerIndex].currentHand[1] = this.getNextCardFromTheDeck();
     }
-    this.setState({players});
-  }
-  buttonPressed = () => {
+    this.setState({ players });
+  };
+  startGameBtnClicked = () => {
+    // deal the pocket hands to all players:
     this.dealCardsToPlayers();
-  }
+
+    // collect blinds
+    let players = this.state.players;
+    let currDealInfo = this.state.currDealInfo;
+    for (let playerIndex = 0; playerIndex < players.length; playerIndex++) {
+      if (players[playerIndex].positionThisDeal == 1) {
+        // This player is the Small Blind
+        players[playerIndex].betThisRound += 1;
+        players[playerIndex].chipStack -= 1;
+      }
+      if (players[playerIndex].positionThisDeal == 2) {
+        // This player is the Big Blind
+        players[playerIndex].betThisRound += 2;
+        players[playerIndex].chipStack -= 2;
+      }
+      if (players[playerIndex].positionThisDeal == 3) {
+        players[playerIndex].isItMyTurn = true;
+        currDealInfo.whoseTurnIsIt = 3;
+      }
+    }
+    // end: collect blinds.
+
+    currDealInfo.highestBet = 2;
+    currDealInfo.currRound = "preflop";
+
+    this.setState({ players });
+    this.setState({ currDealInfo });
+  };
+  raiseBtnPress = () => {
+    let currDealInfo = this.state.currDealInfo;
+    let players = this.state.players;
+    let whoseTurnIsIt = currDealInfo.whoseTurnIsIt;
+
+    // raise the bet from the current player.
+    // and remove that amount from chipstack
+    players[whoseTurnIsIt].betThisRound += 5;
+    players[whoseTurnIsIt].chipStack -= 5;
+
+    currDealInfo.highestBet = players[whoseTurnIsIt].betThisRound;
+
+    this.setState({ players });
+    this.setState({ currDealInfo });
+    this.moveAction();
+  };
+  checkBtnPress = () => {
+    this.moveAction();
+  };
+  foldBtnPress = () => {
+    let currDealInfo = this.state.currDealInfo;
+    let players = this.state.players;
+    let whoseTurnIsIt = currDealInfo.whoseTurnIsIt;
+
+    players[whoseTurnIsIt].hasFolded = true;
+    this.setState({ players });
+    this.moveAction();
+  };
+  callBtnPress = () => {
+    let currDealInfo = this.state.currDealInfo;
+    let players = this.state.players;
+    let whoseTurnIsIt = currDealInfo.whoseTurnIsIt;
+
+    let betDifference =
+      currDealInfo.highestBet - players[whoseTurnIsIt].betThisRound;
+
+    if (betDifference > 0) {
+      players[whoseTurnIsIt].betThisRound += betDifference;
+      players[whoseTurnIsIt].chipStack -= betDifference;
+    }
+    this.setState({ players });
+    this.moveAction();
+  };
+
+  moveAction = () => {
+    let currDealInfo = this.state.currDealInfo;
+    let players = this.state.players;
+
+    // Moves the action from one player to the next after a player has acted.
+    // If the player who acted last is the last player in the round, move to the next round
+
+    // Tell previous player it's not their turn anymore:
+    players[currDealInfo.whoseTurnIsIt].isItMyTurn = false;
+
+    // Check to see if someone has won the game.
+    // which would happen if all players have folded except one.
+    let nonFoldedPlayers = [];
+    for (let playerIndex = 0; playerIndex < players.length; playerIndex++) {
+      if (players[playerIndex].hasFolded == false) {
+        nonFoldedPlayers.push(playerIndex);
+      }
+    }
+    if (nonFoldedPlayers.length == 1) {
+      // only one player left who hasn't folded!
+      // that player is winner!
+      console.log("Winner is: ", players[nonFoldedPlayers[0]].name);
+      // TODO: Disable all buttons.
+      // TODO: End this deal.
+    }
+
+    // increment variable
+    if (currDealInfo.whoseTurnIsIt == players.length - 1) {
+      // this is the last player, move to beginning.
+      currDealInfo.whoseTurnIsIt = 0;
+    } else {
+      currDealInfo.whoseTurnIsIt++;
+    }
+
+    // Folded players:
+    // If the next player has folded run this function again
+    // to move to the next-next player.
+    if (players[currDealInfo.whoseTurnIsIt].hasFolded == true) {
+      this.setState({ players });
+      this.setState({ currDealInfo });
+      this.moveAction();
+    } else {
+      // This player hasn't folded.
+
+      // Update whose turn it is:
+      players[currDealInfo.whoseTurnIsIt].isItMyTurn = true;
+
+      // Add up all this round's bets to get the pot for this round:
+      currDealInfo.currPotRound = 0;
+      for (let playerIndex = 0; playerIndex < players.length; playerIndex++) {
+        currDealInfo.currPotRound += players[playerIndex].betThisRound;
+      }
+
+      this.setState({ players });
+      this.setState({ currDealInfo });
+
+      // Determine if it's time to move to the next round:
+      let timeForNextRound = true;
+      for (let playerIndex = 0; playerIndex < players.length; playerIndex++) {
+        if (players[playerIndex].betThisRound < currDealInfo.highestBet) {
+          timeForNextRound = false;
+        }
+      }
+      if (timeForNextRound) {
+        console.log("Time for next round");
+      }
+    }
+  };
   getNextCardFromTheDeck = () => {
-    // copy deckOfCards from state so we can manipulate it. 
+    // copy deckOfCards from state so we can manipulate it.
     let deckOfCards = this.state.deckOfCards;
-    
+
     let cardIndex = 0;
-    while(deckOfCards[cardIndex].hasBeenDealt == true) {
+    while (deckOfCards[cardIndex].hasBeenDealt == true) {
       cardIndex++;
     }
     let nextCard = deckOfCards[cardIndex];
     deckOfCards[cardIndex].hasBeenDealt = true;
-    this.setState({deckOfCards});
+    this.setState({ deckOfCards });
     return nextCard;
-  }
+  };
   render() {
+    const whoseTurnIsIt = this.state.whoseTurnIsIt;
+    const players = this.state.players;
     return (
       <ScrollView style={styles.mainContainer}>
         <Text>Texas Hold Em Poker</Text>
         <View style={styles.deckOfCards}>
-          {this.state.deckOfCards.map((oneCard) => <PlayingCard key={oneCard.key} suit={oneCard.suit} value={oneCard.value} hasBeenDealt={oneCard.hasBeenDealt} />)}
+          {this.state.deckOfCards.map(oneCard => (
+            <PlayingCard
+              key={oneCard.key}
+              suit={oneCard.suit}
+              value={oneCard.value}
+              hasBeenDealt={oneCard.hasBeenDealt}
+            />
+          ))}
         </View>
-
+        <View style={styles.sharedCards}>
+          {this.state.currDealInfo.sharedCards.map(oneCard => (
+            <PlayingCard
+              key={oneCard.key}
+              suit={oneCard.suit}
+              value={oneCard.value}
+            />
+          ))}
+        </View>
+        <Text>Highest Bet: {this.state.currDealInfo.highestBet}</Text>
+        <Text>Pot (round): {this.state.currDealInfo.currPotRound}</Text>
+        <Text>Pot (center): {this.state.currDealInfo.currPotCenter}</Text>
         <PlayerList players={this.state.players} />
-        <Button title="Deal Cards" onPress={this.buttonPressed} onClick={this.buttonClicked} />
+        // If we are in "pregame" state show the start button:
+        {this.state.currDealInfo.currRound == "pregame" ? (
+          <TouchableOpacity
+            style={styles.startGameBtn}
+            onPress={this.startGameBtnClicked}
+          >
+            <View style={styles.actionButtonView}>
+              <Text style={styles.actionButtonText}>Start Game</Text>
+            </View>
+          </TouchableOpacity>
+        ) : null}
+        // If we are not in "pregame", show the btns for raise, check, call,
+        fold.
+        {this.state.currDealInfo.currRound != "pregame" ? (
+          <View style={styles.actionButtons}>
+            <TouchableOpacity
+              style={styles.actionButton}
+              onPress={this.raiseBtnPress}
+            >
+              <View style={styles.actionButtonView}>
+                <Text style={styles.actionButtonText}>Raise</Text>
+                <MaterialCommunityIcons
+                  name="arrow-up"
+                  style={styles.actionButtonIcon}
+                />
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={
+                this.state.currDealInfo.highestBet ==
+                this.state.players[this.state.currDealInfo.whoseTurnIsIt]
+                  .betThisRound
+                  ? styles.actionButton
+                  : styles.actionButtonDisabled
+              }
+              disabled={
+                this.state.currDealInfo.highestBet ==
+                this.state.players[this.state.currDealInfo.whoseTurnIsIt]
+                  .betThisRound
+                  ? false
+                  : true
+              }
+              onPress={this.checkBtnPress}
+            >
+              <View style={styles.actionButtonView}>
+                <Text style={styles.actionButtonText}>Check</Text>
+                <MaterialCommunityIcons
+                  name="check"
+                  style={styles.actionButtonIcon}
+                />
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={
+                this.state.currDealInfo.highestBet >
+                this.state.players[this.state.currDealInfo.whoseTurnIsIt]
+                  .betThisRound
+                  ? styles.actionButton
+                  : styles.actionButtonDisabled
+              }
+              disabled={
+                this.state.currDealInfo.highestBet >
+                this.state.players[this.state.currDealInfo.whoseTurnIsIt]
+                  .betThisRound
+                  ? false
+                  : true
+              }
+              onPress={this.callBtnPress}
+            >
+              <View style={styles.actionButtonView}>
+                <Text style={styles.actionButtonText}>Call</Text>
+                <MaterialCommunityIcons
+                  name="equal"
+                  style={styles.actionButtonIcon}
+                />
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.actionButton}
+              onPress={this.foldBtnPress}
+            >
+              <View style={styles.actionButtonView}>
+                <Text style={styles.actionButtonText}>Fold</Text>
+                <MaterialCommunityIcons
+                  name="close"
+                  style={styles.actionButtonIcon}
+                />
+              </View>
+            </TouchableOpacity>
+          </View>
+        ) : null}
       </ScrollView>
     );
   }
@@ -130,25 +397,70 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   deckOfCards: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
+    flexDirection: "row",
+    flexWrap: "wrap",
+    marginBottom: 20
+  },
+  actionButtons: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    marginTop: 50
+  },
+  startGameBtn: {
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: "blue",
+    flexGrow: 1,
+    marginTop: 50,
+    marginLeft: 10,
+    marginRight: 10,
+    padding: 10
+  },
+  actionButton: {
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: "blue",
+    flexGrow: 1,
+    marginLeft: 10,
+    marginRight: 10,
+    padding: 10
+  },
+  actionButtonDisabled: {
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: "gainsboro",
+    flexGrow: 1,
+    marginLeft: 10,
+    marginRight: 10,
+    padding: 10
+  },
+  actionButtonView: {
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+  actionButtonText: {
+    color: "blue"
+  },
+  actionButtonIcon: {
+    fontSize: 17,
+    color: "blue"
   },
   mainContainer: {
     padding: 10,
-    marginTop: 50,
+    marginTop: 50
   }
 });
 
-
-
 // Shuffle function grabbed from:
 // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-const shuffle = (array) => {
-  var currentIndex = array.length, temporaryValue, randomIndex;
+const shuffle = array => {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
 
   // While there remain elements to shuffle...
   while (0 !== currentIndex) {
-
     // Pick a remaining element...
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
@@ -160,6 +472,4 @@ const shuffle = (array) => {
   }
 
   return array;
-}
-
-
+};
--- PlayerList.js
+++ PlayerList.js
@@ -1,12 +1,9 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import PlayingCard from './PlayingCard.js';
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import PlayingCard from "./PlayingCard.js";
 
 export default class PlayerList extends React.Component {
-
-  state = {
-  
-  }
+  state = {};
 
   constructor() {
     super();
@@ -16,23 +13,89 @@ export default class PlayerList extends React.Component {
     const styles = StyleSheet.create({
       player: {
         marginTop: 20,
+        flexDirection: "row",
+        flexWrap: "wrap"
+      },
+      playerText: {
+        fontFamily: "Courier",
+        marginRight: 20
+      },
+      folded: {
+        opacity: 0.2
+      },
+      currTurn: {
+        borderWidth: 1,
+        paddingTop: 10,
+        paddingBottom: 10,
+        backgroundColor: "cornsilk",
+        borderColor: "gainsboro",
+        borderRadius: 5
+      },
+      playingCards: {
+        flexDirection: "row",
+        flexWrap: "wrap"
       }
     });
 
     const playerList = this.props.players.map(player => {
-      if(player.currentHand[0] !== undefined) {
-        return <View style={styles.player} key={player.key}><Text>{player.name}: ${player.chipStack}: ${player.betThisRound}</Text><PlayingCard suit={player.currentHand[0].suit} value={player.currentHand[0].value} /><PlayingCard suit={player.currentHand[1].suit} value={player.currentHand[1].value} /></View>
-      } else {
-        return <View style={styles.player} key={player.key}><Text>{player.name}: ${player.chipStack}: ${player.betThisRound}</Text></View> 
+      let playerInfo = [];
+      let playerStyles = [styles.player];
+      let positionIndicator = "";
+
+      if (player.isItMyTurn) {
+        playerStyles = [...playerStyles, styles.currTurn];
       }
+
+      if (player.hasFolded) {
+        playerStyles = [...playerStyles, styles.folded];
+      }
+
+      switch (player.positionThisDeal) {
+        case 0:
+          positionIndicator = " D ";
+          break;
+        case 1:
+          positionIndicator = " SB";
+          break;
+        case 2:
+          positionIndicator = " BB";
+          break;
+        default:
+          positionIndicator = "   ";
+          break;
+      }
+
+      if (player.currentHand[0] !== undefined) {
+        playerInfo.push(
+          <View style={playerStyles} key={player.key}>
+            <Text style={styles.playerText}>
+              {positionIndicator} : {player.name} : ${player.chipStack} : $
+              {player.betThisRound} :
+            </Text>
+            <PlayingCard
+              suit={player.currentHand[0].suit}
+              value={player.currentHand[0].value}
+            />
+            <PlayingCard
+              suit={player.currentHand[1].suit}
+              value={player.currentHand[1].value}
+            />
+          </View>
+        );
+      } else {
+        playerInfo.push(
+          <View style={playerStyles} key={player.key}>
+            <Text style={styles.playerText}>
+              {positionIndicator} : {player.name} : ${player.chipStack} : $
+              {player.betThisRound}
+            </Text>
+          </View>
+        );
+      }
+
+      return playerInfo;
     });
-    
-  	return (
-  		<View>
-        {playerList}
-    	</View>
-    );
-    
+
+    return <View>{playerList}</View>;
   }
-  
 }
